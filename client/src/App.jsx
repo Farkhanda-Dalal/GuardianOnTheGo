b73c3d48839da0c0cf3bea1a9bb95e79bddd2fe4
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2025-02-08 21:48:13 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,37 @@
 import React from "react";
-import axios from "axios"
+import axios from "axios";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import {Toaster} from 'react-hot-toast'
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
+import AdminHome from "./components/AdminHome";
+import ChildProfileForm from "./pages/ChildProfile";
+import AddCoGuardians from "./pages/AddCoGuardians";
 
-axios.defaults.baseURL='http://localhost:8080';
-axios.defaults.withCredentials=true;
-
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.withCredentials = true;
 
 const App = () => {
+  const location = useLocation();
+
+  // Show Navbar only for these routes
+  const showNavbarRoutes = ["/", "/register", "/login"];
+
   return (
-    <> 
-      <Navbar></Navbar>
-      <Toaster position="top" toastOptions={{duration:3000}}></Toaster>
+    <>
+      {showNavbarRoutes.includes(location.pathname) && <Navbar />}
+      <Toaster position="top" toastOptions={{ duration: 3000 }} />
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/adminHome" element={<AdminHome />} />
+        <Route path="/addChildProfile" element={<ChildProfileForm />} />
+        <Route path="/addCo-Guardians" element={<AddCoGuardians />} />
+
       </Routes>
     </>
   );
